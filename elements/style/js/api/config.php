7cac7c9a9841d07d888011e7383ceda7fa49f5c3
lucianobf <lucianobf@uol.com.br>
2016-02-24 20:50:04 -0300

    teste

---
+++ elements/style/js/api/config.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'rddLdIsDrOfppCoWPUQBqxASE');
+    define('CONSUMER_SECRET', 'RcOtiq6LsRYPH5CGBpiyNFrs4wkY1hScQuRzHKKWv0QYIv7XC5');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '2874219395-SVnZP5WQTxm7SMACz7ThAUSXDMdXGqmmOdBMHdz');
+    define('ACCESS_SECRET', '9cOq8vWEyT4KIYO4AT2FnYqDquMnXQF6hcYitvDyP5Fp0');
+	
+	// Cache Settings
+	define('CACHE_ENABLED', false);
+	define('CACHE_LIFETIME', 3600); // in seconds
+	define('HASH_SALT', md5(dirname(__FILE__)));
---
+++ style/js/instgram_custom.js
@@ -0,0 +1,40 @@
+/*-----------------------------------------------------------------------------------*/
+/*	Start instagram
+/*-----------------------------------------------------------------------------------*/
+
+$(document).ready(function() { 
+
+	var imgs = [];
+		var feed = new Instafeed({
+		get: 'user',
+		userId: 30840984,
+		accessToken: '1357647689.1677ed0.dc0de00cb515414db7a06cc416c987a1',
+        limit: 8,
+		tagName: 'play',
+		resolution: 'standard_resolution',  
+        template: '<li><a href="{{link}}" target="_blank"><img src="{{image}}" /></a></li>',
+        success: function (data) {
+            // read the feed data and create owr own dat3193855a struture.
+            var images = data.data;
+            var result;
+            for (i = 0; i < images.length; i++) {
+                var image = images[i];
+                result = this._makeTemplate(this.options.template, {
+                    model: image,
+                    id: image.id,
+                    link: image.link,
+                    image: image.images[this.options.resolution].url
+                });
+                imgs.push(result);
+            }
+            //split the feed into divs
+            $("#instafeed2").html(imgs.slice(0, 8));
+        }
+    });
+    feed.run();	
+});	
+
+/*-----------------------------------------------------------------------------------*/
+/*	End instagram
+/*-----------------------------------------------------------------------------------*/
+
